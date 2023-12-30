1326426bae8c9c263b798ae5fc2beed4ccdb4832
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-12-30 22:07:20 +0530

    Department Entity

---
+++ src/main/java/com/EmployeeManagement/entity/Department.java
@@ -0,0 +1,25 @@
+package com.EmployeeManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "emp_ms_department")
+public class Department {
+    @Id
+    @Column(name = "department_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer departmentId;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String description;
+}
