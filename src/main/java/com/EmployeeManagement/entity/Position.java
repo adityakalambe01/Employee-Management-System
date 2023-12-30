cc75efb1b478215256a32c49522a9e6bc950750f
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-12-30 22:09:39 +0530

    Position/Job Title Entity

---
+++ src/main/java/com/EmployeeManagement/entity/Position.java
@@ -0,0 +1,28 @@
+package com.EmployeeManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "emp_ms_position")
+public class Position {
+    @Id
+    @Column(name = "position_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer positionId;
+
+    @Column(name = "position_name")
+    private String positionName;
+
+    @Column(name = "responsibilities")
+    private String responsibilities;
+
+    @Column(name = "salary_range")
+    private Double salaryRange;
+}
