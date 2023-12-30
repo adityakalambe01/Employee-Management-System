092e005d30d19a2df1000e1d7bd6357334cdd148
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-12-30 22:09:04 +0530

    Manager/Supervisor Entity

---
+++ src/main/java/com/EmployeeManagement/entity/Manager.java
@@ -0,0 +1,29 @@
+package com.EmployeeManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "emp_ms_manager")
+public class Manager {
+    @Id
+    @Column(name = "manager_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer managerId;
+
+    @Column(name = "employee_id")
+    private Integer employeeId;
+
+    @ElementCollection
+    @CollectionTable(name = "emp_ms_team_managed", joinColumns = @JoinColumn(name = "manager_id"))
+    @Column(name = "employee_id")
+    private List<Long> teamManaged;
+}
