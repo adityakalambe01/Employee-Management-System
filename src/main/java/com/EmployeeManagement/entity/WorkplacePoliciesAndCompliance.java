8cfa40d789d5e2547e29981da58e9c5eeea4fcab
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-12-30 22:10:16 +0530

    Workplace Policies and Compliance Entity

---
+++ src/main/java/com/EmployeeManagement/entity/WorkplacePoliciesAndCompliance.java
@@ -0,0 +1,28 @@
+package com.EmployeeManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "emp_ms_workplace_policies_and_compliance")
+public class WorkplacePoliciesAndCompliance {
+    @Id
+    @Column(name = "policy_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer policyId;
+
+    @Column(name = "policy_name")
+    private String policyName;
+
+    @Column(name = "description")
+    private String policyDescription;
+
+    @Column(name = "compliance_status")
+    private Boolean complianceStatus;
+}
