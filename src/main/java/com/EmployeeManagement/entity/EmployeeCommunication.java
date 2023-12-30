dd89820b3a4524c1d4f51176198369dac79f79bf
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-12-30 22:07:52 +0530

    Employee Communication Entity

---
+++ src/main/java/com/EmployeeManagement/entity/EmployeeCommunication.java
@@ -0,0 +1,35 @@
+package com.EmployeeManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "emp_ms_")
+public class EmployeeCommunication {
+    @Id
+    @Column(name = "message_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer messageId;
+
+    @Column(name = "sender_id")
+    private Integer senderId;
+
+    @Column(name = "receiver_id")
+    private Integer receiverId;
+
+    @Column(name = "message_type")
+    private String messageType;
+
+    @Column(name = "message_content")
+    private String messageContent;
+
+    @Column(name = "message_timestamp")
+    private LocalDateTime messageTimestamp;
+}
