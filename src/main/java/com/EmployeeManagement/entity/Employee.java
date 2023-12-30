b42287e9e58f87b7899119e1210f1e89410cdeb6
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-12-30 22:07:32 +0530

    Employee Entity

---
+++ src/main/java/com/EmployeeManagement/entity/Employee.java
@@ -0,0 +1,45 @@
+package com.EmployeeManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="emp_ms_employee")
+public class Employee {
+    @Id
+    @Column(name = "employee_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int employeeId;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "mobile_number")
+    private Long mobileNumber;
+    @Column(name = "email_id")
+    private String emailId;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "joining_date")
+    private LocalDate joiningDate;
+    @Column(name = "department_id")
+    private Integer departmentId;
+    @Column(name = "position_id")
+    private Integer positionId;
+    @Column(name = "salary")
+    private Double salary;
+
+
+}
