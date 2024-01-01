0ed5ed7b2f4924ec2d655fb930cff0f51b336a2b
AdityaKalambe01 <adityakalambe20@gmail.com>
2024-01-01 21:42:41 +0530

    Employee Entity

--- src/main/java/com/EmployeeManagement/entity/Employee.java
+++ src/main/java/com/EmployeeManagement/entity/Employee.java
@@ -16,28 +16,38 @@ public class Employee {
     @Id
     @Column(name = "employee_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int employeeId;
+    private Integer employeeId;
 
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
+
     @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+
     @Column(name = "gender")
     private String gender;
+
     @Column(name = "mobile_number")
     private Long mobileNumber;
+
     @Column(name = "email_id")
     private String emailId;
+
     @Column(name = "address")
     private String address;
+
     @Column(name = "joining_date")
     private LocalDate joiningDate;
+
     @Column(name = "department_id")
     private Integer departmentId;
+
     @Column(name = "position_id")
     private Integer positionId;
+
     @Column(name = "salary")
     private Double salary;
 
