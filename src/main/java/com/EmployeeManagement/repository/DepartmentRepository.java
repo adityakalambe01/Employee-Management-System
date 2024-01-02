6a9812202373170ba9fc2a1d2549a0677e4dc822
AdityaKalambe01 <adityakalambe20@gmail.com>
2024-01-02 20:14:59 +0530

    Repositories

--- src/main/java/com/EmployeeManagement/repository/DepartmentRepository.java
+++ src/main/java/com/EmployeeManagement/repository/DepartmentRepository.java
@@ -2,11 +2,19 @@ package com.EmployeeManagement.repository;
 
 import com.EmployeeManagement.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
     public List<Department> findByDepartmentNameLike(String departmentName);
+
+    @Query("select d from Department d order by d.departmentName")
+    List<Department> findOrderByDepartmentNameAsc();
+
+    @Query("select d from Department d order by d.departmentName desc ")
+    List<Department> findOrderByDepartmentNameDesc();
+
+
 }
--- src/main/java/com/EmployeeManagement/repository/EmployeeRepository.java
+++ src/main/java/com/EmployeeManagement/repository/EmployeeRepository.java
@@ -2,8 +2,27 @@ package com.EmployeeManagement.repository;
 
 import com.EmployeeManagement.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findByFirstNameLikeAndLastNameLike(String firstName, String LastName);
+    List<Employee> findBySalaryBetween(Double salary, Double salary2);
+
+    List<Employee> findByJoiningDateBetween(LocalDate startYear, LocalDate endYear);
+
+//    @Query("select e from Employee e where e.gender=:gender")
+    List<Employee> findByGender(@Param("gender") String gender);
+
+    @Query("select e from Employee e order by e.salary")
+    List<Employee> findByOrderSalaryAsc();
+
+    @Query("select e from Employee e order by e.salary desc")
+    List<Employee> findByOrderSalaryDesc();
 }
