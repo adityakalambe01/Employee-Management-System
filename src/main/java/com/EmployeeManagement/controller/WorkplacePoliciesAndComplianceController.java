3e375042cb4b67c595fa13d489cc8670a6029cf6
AdityaKalambe01 <adityakalambe20@gmail.com>
2024-01-11 00:20:48 +0530

    Final Commit

--- src/main/java/com/EmployeeManagement/controller/WorkplacePoliciesAndComplianceController.java
+++ src/main/java/com/EmployeeManagement/controller/WorkplacePoliciesAndComplianceController.java
@@ -1,9 +1,29 @@
 package com.EmployeeManagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.EmployeeManagement.entity.WorkplacePoliciesAndCompliance;
+import com.EmployeeManagement.service.WorkplacePoliciesAndComplianceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/WorkplacePoliciesAndCompliance/")
 public class WorkplacePoliciesAndComplianceController {
+    @Autowired
+    WorkplacePoliciesAndComplianceService workplacePoliciesAndComplianceService;
+
+    @PostMapping("add/policy")
+    public boolean addPolicyIntoDb(@RequestBody WorkplacePoliciesAndCompliance workplacePoliciesAndCompliance){
+        return workplacePoliciesAndComplianceService.addPolicyIntoDb(workplacePoliciesAndCompliance);
+    }
+
+    @PutMapping("update/policy/id{policyId}")
+    public Boolean updatePolicyFromDb(@PathVariable Integer policyId,
+                                      @RequestBody WorkplacePoliciesAndCompliance updatedWorkplacePoliciesAndCompliance){
+        return workplacePoliciesAndComplianceService.updatePolicyFromDb(policyId, updatedWorkplacePoliciesAndCompliance);
+    }
+
+    @DeleteMapping("delete/policy/id/{policyId}")
+    public Boolean deletePolicyFromDb(@PathVariable Integer policyId){
+        return workplacePoliciesAndComplianceService.deletePolicyFromDb(policyId);
+    }
 }
